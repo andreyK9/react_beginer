cf98f0a2771b64e88df3e25f7f5cc8c3358478d4
AndreyK9 <a.kovalenko@onpoint.ru>
2018-06-05 17:39:04 +0300

    begit lesson 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import {render} from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function Article() {
+  const body = <section>body</section>
+  return (
+    <div className='hello' style={{color: red}}>
+      <h2>title</h2>
+      {body}
+      <h3>creation date: {(new Date()).toDateString()}</h3>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>App name</h1>
+      <Article />
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'));
